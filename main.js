5b226d92dc3561641271c2ef167d981187d78545
Fatimamehmoodali1 <162306190+Fatimamehmoodali1@users.noreply.github.com>
2024-03-06 00:18:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+//Q2:
+//let personName="Fatima";
+//console.log('hello',personName,"would you like to learn some typescript today");
+//Q3:
+//let personName="Fatima";
+//console.log(personName.toLowerCase());
+//console.log(personName.toUpperCase());
+//console.log(personName.charAt(0).toUpperCase()+personName.slice(1).toLowerCase());
+//Q4:
+//console.log("Audrey Hypburn once said",'"Word IMPOSSIBLE itself says I AM Possible"');
+//Q5:
+//let famousePerson="Audrey Hypburn";
+//let message="once said,'Word IMPOSSIBLE itself says I AM POSSIBLE'";
+//console.log(famousePerson,message);
+//Q6:
+//let whiteSpace="\n\t Fatima Mehmoodali\t\n";
+//console.log(whiteSpace);
+//let withoutwhiteSpace=whiteSpace.trim();
+//console.log(withoutwhiteSpace);
